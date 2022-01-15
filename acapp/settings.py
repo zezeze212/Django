9c344099085f47f3a55395ae71e37f5049578b05
zetao An <1016695608@qq.com>
2022-01-15 12:42:32 +0800

    implement multiplayer 1

--- acapp/settings.py
+++ acapp/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["59.110.242.248","app641.acapp.acwing.com.cn"]
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'game.apps.GameConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -138,3 +139,16 @@ MEDIA_URL = '/mdeia/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ASGI_APPLICATION = 'acapp.asgi.application'
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
+ROOM_CAPACITY = 3
--- static/js/src/playground/player/zbase.js
+++ static/js/src/playground/player/zbase.js
@@ -1,5 +1,8 @@
 class Player extends AcGameObject {
-    constructor(playground, x, y, radius, color, speed, is_me) {
+    constructor(playground, x, y, radius, color, speed, character, username, photo) {
+
+        console.log(character, username, photo);
+
         super();
         this.playground = playground;
         this.ctx = this.playground.game_map.ctx;
@@ -14,25 +17,26 @@ class Player extends AcGameObject {
         this.radius = radius;
         this.color = color;
         this.speed = speed;
-        this.is_me = is_me;
+        this.character = character;
+        this.username = username;
+        this.photo = photo;
         this.eps = 0.01;
         this.friction = 0.9;
         this.spent_time = 0;
 
         this.cur_skill = null;
 
-        if(this.is_me) {
+        if(this.character !== "robot") {
             this.img = new Image();
-            console.log(this.playground.root.settings.photo);
-            this.img.src = this.playground.root.settings.photo;
+            this.img.src = this.photo;
         }
 
     }
 
     start() {
-        if (this.is_me) {
+        if (this.character === "me") {
             this.add_listening_events();
-        } else {
+        } else if (this.character === "robot"){
             let tx = Math.random() * this.playground.width / this.playground.scale;
             let ty = Math.random() * this.playground.height / this.playground.scale;
             this.move_to(tx, ty);
@@ -128,7 +132,7 @@ class Player extends AcGameObject {
 
     update_move() {//更新玩家移动
         this.spent_time += this.timedelta / 1000;
-        if(!this.is_me) {
+        if(this.character === "robot") {
             if(Math.random() < 1 / 200.0 && this.spent_time > 3 ) {
                 let player = this.playground.players[Math.floor(Math.random() * this.playground.players.length)];
                 let tx = player.x + player.speed * this.vx * this.timedelta /1000 * 0.3;
@@ -149,7 +153,7 @@ class Player extends AcGameObject {
             if (this.move_length < this.eps) {
                 this.move_length = 0;
                 this.vx = this.vy = 0;
-                if (!this.is_me) {
+                if (this.character === "robot") {
                     let tx = Math.random() * this.playground.width / this.playground.scale;
                     let ty = Math.random() * this.playground.height / this.playground.scale;
                     this.move_to(tx, ty);
@@ -168,7 +172,7 @@ class Player extends AcGameObject {
 
     render() {
         let scale = this.playground.scale;
-        if (this.is_me) {
+        if (this.character !== "robot") {
             this.ctx.save();
             this.ctx.beginPath();
             this.ctx.arc(this.x * scale, this.y * scale, this.radius * scale, 0, Math.PI * 2, false);
