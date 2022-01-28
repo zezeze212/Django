7fb47b9f6de845e27fd84f2ffd1fc65f854a3976
zetao An <1016695608@qq.com>
2022-01-28 12:20:06 +0800

    implement thrift

--- game/consumers/multiplayer/index.py
+++ game/consumers/multiplayer/index.py
@@ -3,66 +3,65 @@ import json
 from django.conf import settings
 from django.core.cache import cache
 
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from match_system.src.match_server.match_service import Match
+from game.models.player.player import Player
+from channels.db import database_sync_to_async
+
+
+
 class MultiPlayer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_name, self.channel_name);
+        if self.room_name:
+            await self.channel_layer.group_discard(self.room_name, self.channel_name)
 
 
     async def create_player(self, data):
         self.room_name = None
+        self.uuid = data['uuid']
 
-        start = 0;
-        if data['username'] != "阿泽.":
-            start = 100000;
+        # Make socket
+        transport = TSocket.TSocket('127.0.0.1', 9090)
 
-        for i in range(start, 100000000):
-            name = "room-%d" % (i)
-            if not cache.has_key(name) or len(cache.get(name)) < settings.ROOM_CAPACITY:
-                self.room_name = name
-                break
+        # Buffering is critical. Raw sockets are very slow
+        transport = TTransport.TBufferedTransport(transport)
 
-        if not self.room_name:
-            return
+        # Wrap in a protocol
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+        # Create a client to use the protocol encoder
+        client = Match.Client(protocol)
+
+        def db_get_player():
+            return Player.objects.get(user__username=data['username']) #注意两个下划线
+
+        player = await database_sync_to_async(db_get_player)() #注意写法，奇怪的写法
+
+        # Connect!
+        transport.open()
+
+        client.add_player(player.score, data['uuid'], data['username'], data['photo'], self.channel_name)
+
+        # Close!
+        transport.close()
 
 
-
-        if not cache.has_key(self.room_name):
-            cache.set(self.room_name, [], 3600) #对战时间1小时
-
-        for player in cache.get(self.room_name):
-            await self.send(text_data=json.dumps({
-                'event': "create_player",
-                'uuid': player['uuid'],
-                'username': player['username'],
-                'photo': player['photo'],
-            }))
-
-        await self.channel_layer.group_add(self.room_name, self.channel_name)
-
-        players = cache.get(self.room_name)
-        players.append({
-            'uuid': data['uuid'],
-            'username': data['username'],
-            'photo': data['photo'],
-        })
-        cache.set(self.room_name, players, 3600) #最后一名玩家加入后有效期1小时
-        await self.channel_layer.group_send( #组内成员发送信息
-            self.room_name,
-            {
-                'type': "group_send_event",
-                'event': "create_player",
-                'uuid': data['uuid'],
-                'username': data['username'],
-                'photo': data['photo'],
-
-            }
-        )
 
     async def group_send_event(self, data):
+        if not self.room_name:
+            keys = cache.keys('*%s*' % (self.uuid)) #用cache中的函数查找该玩家的房间
+            if keys:
+                self.room_name = keys[0] #记录房间名字
         await self.send(text_data=json.dumps(data))
+
+
 
     async def move_to(self, data):
         await self.channel_layer.group_send(
@@ -75,7 +74,8 @@ class MultiPlayer(AsyncWebsocketConsumer):
                 'ty': data['ty'],
             }
         )
-    
+
+
     async def shoot_fireball(self, data):
         await self.channel_layer.group_send(
             self.room_name,
